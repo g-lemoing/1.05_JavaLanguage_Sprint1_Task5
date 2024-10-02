c52e27142b032256d639d799a73cdd8ce1f052b4
g-lemoing <le.moing.gwenael@gmail.com>
2024-10-02 12:08:51 +0200

    Correccions N1Ex2

--- src/nivell1/exercise2/java/com/main/Main.java
+++ src/nivell1/exercise2/java/com/main/Main.java
@@ -4,17 +4,34 @@ import nivell1.exercise2.java.com.modules.ArbreArxius;
 
 import java.io.Console;
 import java.io.File;
-import java.nio.file.Paths;
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main (String[]args){
+    public static void main (String[]args) {
         Console c = System.console();
+        String dir = "";
         if (c == null){
             System.err.println("Console not available");
             System.exit(1);
         }
-        String dir = c.readLine("Indica el directori relatiu del qual vols llistar-ne el contingut,\nprem directament Intro per llistar el directori actual: \n");
-        dir = dir.isEmpty()? "." + File.separator : dir;
-        ArbreArxius.listFiles(Paths.get(dir));
+
+        if (args.length > 0){
+            dir = args[0];
+        }
+        else{
+            System.out.println("Ha d'entrar un nom de directori.");
+            System.exit(1);
+        }
+//        String dir = c.readLine("Indica el directori relatiu del qual vols llistar-ne el contingut,\nprem directament Intro per llistar el directori actual: \n");
+//        dir = dir.isEmpty()? "." + File.separator : dir;
+        try{
+            if(ArbreArxius.checkDir(dir)){
+                System.out.println("El directori " + dir + " té el contingut següent:");
+                ArbreArxius.listFiles(new File(dir), 1);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/nivell1/exercise2/java/com/modules/ArbreArxius.java
+++ src/nivell1/exercise2/java/com/modules/ArbreArxius.java
@@ -1,31 +1,43 @@
 package nivell1.exercise2.java.com.modules;
 
-import java.io.IOException;
-import java.nio.file.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 public class ArbreArxius {
 
-    public static void listFiles(Path relDir){
-
-        try(DirectoryStream<Path> stream = Files.newDirectoryStream(relDir)){
-            System.out.println("Aquest és el contingut del directori: " + relDir);
-            for(Path elem: stream){
-                if(Files.isDirectory(elem)){
-                    String line = " D - " +
-                            elem.getFileName() + ", modificat el " + Files.getLastModifiedTime(elem);
+    public static boolean checkDir(String relDir) throws FileNotFoundException {
+        File dir = new File(relDir);
+        if(!dir.isDirectory()){
+            throw new FileNotFoundException("El directori no existeix.");
+        }
+        return true;
+    }
+    public static void listFiles(File dir, int level){
+        File[] files = dir.listFiles();
+        if(files != null){
+            Arrays.sort(files);
+            for(File file: files){
+                if (file.isDirectory()){
+                    String line = buildLine(file.getName(), "D", getUpdateTime(file), level);
                     System.out.println(line);
-                    listFiles(elem);
+                    listFiles(file, level + 1);
                 }
-                else{
-                    String line = " F - " +
-                            elem.getFileName()+ ", modificat el " + Files.getLastModifiedTime(elem);
+                else {
+                    String line = buildLine(file.getName(), "F", getUpdateTime(file), level);
                     System.out.println(line);
                 }
             }
         }
-        catch (IOException | DirectoryIteratorException ex){
-            System.err.println(ex.getMessage());
-        }
     }
 
+    public static String buildLine (String fileName, String type, String updateTime, int level){
+        return " ".repeat(level) + type + " - " + fileName + ", modificat el " + updateTime;
+    }
+
+    public static String getUpdateTime(File file){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(file.lastModified());
+    }
 }
