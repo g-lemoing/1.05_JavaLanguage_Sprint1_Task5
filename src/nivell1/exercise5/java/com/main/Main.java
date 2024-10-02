4b6433c76b138edc001f74663b8a3912ac2e7b59
g-lemoing <le.moing.gwenael@gmail.com>
2024-10-02 13:25:41 +0200

    Correccions N1Ex5

--- src/nivell1/exercise5/java/com/main/Main.java
+++ src/nivell1/exercise5/java/com/main/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         final String OUTPUT_FILE = "TestFolder" + File.separator +
                 "Exercise5" + File.separator + "customer.ser";
+
         Customer customer = new Customer("Joan", 25, "Barcelona");
         System.out.println("Instanciem un client: " + customer);
         System.out.println("Serialitzem l'objecte al fitxer " + OUTPUT_FILE);
