b13f761588cc5662f0c5dd6859446ab5eb237000
g-lemoing <le.moing.gwenael@gmail.com>
2024-10-07 11:06:33 +0200

    Modif Nivell2 Ex1

--- src/nivell2/exercise1/java/com/main/Main.java
+++ src/nivell2/exercise1/java/com/main/Main.java
@@ -1,10 +1,10 @@
 package nivell2.exercise1.java.com.main;
 
-import nivell1.exercise3.java.com.modules.ArbreArxius;
+import nivell2.exercise1.java.com.modules.ArbreArxius;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
@@ -12,12 +12,13 @@ public class Main {
 
     public static void main (String[]args){
         Properties properties = new Properties();
-        String OUTPUT_FILE = "";
-        String INPUT_DIRECTORY = "";
+        String outputFile = "";
+        String inputDir = "";
+
         try{
             properties.load(new FileInputStream(CONFIG_PATH));
-            OUTPUT_FILE = properties.getProperty("OUTPUT_FILE");
-            INPUT_DIRECTORY = properties.getProperty("INPUT_DIRECTORY");
+            outputFile = properties.getProperty("OUTPUT_FILE");
+            inputDir = properties.getProperty("INPUT_DIRECTORY");
         }
         catch (FileNotFoundException e){
             System.out.println(e.getMessage());
@@ -30,9 +31,18 @@ public class Main {
             System.err.println("Console not available");
             System.exit(1);
         }
-        String dir = INPUT_DIRECTORY.isEmpty()? "." + File.separator : INPUT_DIRECTORY;
-        ArbreArxius.listFiles(Paths.get(dir), new ArrayList<>(), OUTPUT_FILE);
-        System.out.println("El contingut del directori " + dir + " i dels seus subdirectoris "
-        + "s'ha desat al fitxer " + OUTPUT_FILE);
+
+        String dir = inputDir.isEmpty()? "." + File.separator : inputDir;
+        try{
+            if(ArbreArxius.checkDir(dir)){
+                List<String> contents;
+                contents = ArbreArxius.listFiles(new File(dir), new ArrayList<>(), 0);
+                ArbreArxius.writeToTextFile(outputFile, contents);
+                System.out.println("El contingut del directori " + dir + " i dels seus subdirectoris "
+                        + "s'ha desat al fitxer " + outputFile);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
